ce3c118877fa7e17c83983b49f7568dc5abc8669
asdffg123aaak-spec <asdffg123aaak@gmail.com>
2026-02-15 18:19:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  token: "", // توكن البوت
-    prefix: "", // بريفكس البوت
-    allowedRoleId: "", // ايدي رتبة المسؤولين عن السجن
-    jailRoleId: "", // ايدي رتبة السجن
-    jailRoomId: "", // ايدي روم شات السجن او اي روم خاص فيهم
-    jailLogId: "", // ايدي اللوق حق السجن
+  token: "MTQ3MjU5NTUzMDIwMTY5NDIwOA.Gkpskk.SquqpwldjCBMfBcFKMdAdYbqoqOjt6LaXdQldc", // توكن البوت
+    prefix: "!", // بريفكس البوت
+    allowedRoleId: "1461226327704076300", // ايدي رتبة المسؤولين عن السجن
+    jailRoleId: "1461220631864545498", // ايدي رتبة السجن
+    jailRoomId: "1461140043069391052", // ايدي روم شات السجن او اي روم خاص فيهم
+    jailLogId: "1461840684381241345", // ايدي اللوق حق السجن
 }
